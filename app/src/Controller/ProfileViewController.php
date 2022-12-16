72d46d755c277acdb683332a15775f2a1508ea0a
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-12-16 23:36:48 +0400

    ref: Optimize query for page View user profile with posts.

--- app/src/Controller/ProfileViewController.php
+++ app/src/Controller/ProfileViewController.php
@@ -4,15 +4,24 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Repository\MicroPostRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Uid\Ulid;
 
 class ProfileViewController extends AbstractController
 {
     #[Route('/profile/{id}/view', name: 'app_profile_view')]
-    public function index(User $user, MicroPostRepository $microPostRepository): Response
+    public function index(string $id, UserRepository $userRepository, MicroPostRepository $microPostRepository): Response
     {
+        $user = $userRepository->getUserForUserProfilePage(Ulid::fromRfc4122($id));
+
+        if (null === $user) {
+            throw new NotFoundHttpException('User not found');
+        }
+
         return $this->render('@main/profile_view/index.html.twig', [
             'user' => $user,
             'posts' => $microPostRepository->getPostsByAuthor($user)
--- app/src/Repository/UserRepository.php
+++ app/src/Repository/UserRepository.php
@@ -4,10 +4,12 @@ namespace App\Repository;
 
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+use Symfony\Component\Uid\Ulid;
 
 /**
  * @extends ServiceEntityRepository<User>
@@ -54,5 +56,65 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $user->setPassword($newHashedPassword);
 
         $this->save($user, true);
+    }
+
+    public function getUserForUserProfilePage(User|Ulid $user): ?User
+    {
+        $ulid = $user instanceof User ? $user->getId()->toRfc4122() : $user->toRfc4122();
+
+        return $this->getUserQuery(
+            withProfile: true,
+            withPosts: true,
+            withFollowers: true,
+            withFollowing: true
+        )
+            ->where('u = :user')
+            ->setParameter(':user', $ulid)
+            ->getQuery()
+            ->getSingleResult();
+    }
+
+    private function getUserQuery(
+        bool $withProfile = false,
+        bool $withComments = false,
+        bool $withLikes = false,
+        bool $withPosts = false,
+        bool $withFollowers = false,
+        bool $withFollowing = false
+    ): QueryBuilder
+    {
+        $query = $this->createQueryBuilder('u');
+
+        if ($withProfile) {
+            $query->leftJoin('u.userProfile', 'userProfile')
+                ->addSelect('userProfile');
+        }
+
+        if ($withComments) {
+            $query->leftJoin('u.comments', 'comments')
+                ->addSelect('comments');
+        }
+
+        if ($withLikes) {
+            $query->leftJoin('u.likedPosts', 'likedPosts')
+                ->addSelect('likedPosts');
+        }
+
+        if ($withPosts) {
+            $query->leftJoin('u.microPosts', 'microPosts')
+                ->addSelect('microPosts');
+        }
+
+        if ($withFollowers) {
+            $query->leftJoin('u.followers', 'followers')
+                ->addSelect('followers');
+        }
+
+        if ($withFollowing) {
+            $query->leftJoin('u.following', 'following')
+                ->addSelect('following');
+        }
+
+        return $query;
     }
 }
