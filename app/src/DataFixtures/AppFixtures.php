8ff2f64aa15268e63065db8a2a7210f93576efd2
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-12-17 15:54:49 +0400

    feat: Load avatar image for user profile in AppFixtures

--- app/src/DataFixtures/AppFixtures.php
+++ app/src/DataFixtures/AppFixtures.php
@@ -5,20 +5,25 @@ namespace App\DataFixtures;
 use App\Entity\MicroPost;
 use App\Entity\User;
 use App\Entity\UserProfile;
+use App\Service\SetAvatarImageInterface;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\DependencyInjection\ParameterBag\ContainerBagInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
     public function __construct(
-        private UserPasswordHasherInterface $passwordHasher
+        private UserPasswordHasherInterface $passwordHasher,
+        private SetAvatarImageInterface     $setAvatarImage,
+        private ContainerBagInterface       $containerBag
     )
     {
     }
 
-    public function load(ObjectManager $manager): void
+    public
+    function load(ObjectManager $manager): void
     {
         $faker = Factory::create();
 
@@ -34,8 +39,10 @@ class AppFixtures extends Fixture
         $fixtureUsers[] = new UserFixtureDto('editor@email.com', ['ROLE_EDITOR'], 'Editor ✍', true);
         $fixtureUsers[] = new UserFixtureDto('monkey@email.com', [], 'Monkey man 🐵', true);
         $fixtureUsers[] = new UserFixtureDto('user@email.com', [], 'User 😑', false);
+        $avatarFixturesImagesDir = dirname(__FILE__) . '/profile_images/';
+        $publicDirectoryProfileImages = $this->containerBag->get('micro_post.profile_images_dir');
 
-        foreach ($fixtureUsers as $userDto) {
+        foreach ($fixtureUsers as $index => $userDto) {
             $profile = null;
 
             $profile = (new UserProfile())
@@ -49,13 +56,26 @@ class AppFixtures extends Fixture
                 ->setRoles($userDto->roles);
             $user->setPassword($this->passwordHasher->hashPassword($user, $userPassword));
 
+            $avatarFixtureImage = $avatarFixturesImagesDir . $index . '.jpg';
+            $avatarFile = new \SplFileInfo($avatarFixtureImage);
+
+            if ($avatarFile->isFile()) {
+                $this->setAvatarImage->set(
+                    publicDirectoryProfileImages: $publicDirectoryProfileImages,
+                    file: $avatarFile->getRealPath(),
+                    fileExtension: $avatarFile->getExtension(),
+                    user: $user,
+                    moveFile: false
+                );
+            }
+
             $manager->persist($user);
             $users[] = $user;
         }
 
         $manager->flush();
 
-        for ($i = 0; $i < 10; $i++) {
+        for ($i = 0; $i < 100; $i++) {
             $microPost = (new MicroPost())
                 ->setContent($faker->realTextBetween(5, 140))
                 ->setAuthor($users[array_rand($users)]);
