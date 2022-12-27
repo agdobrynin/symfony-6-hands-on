28d067ee1f169147600686fd453492789451d4f8
Alexey Dobrynin <ag.dobrynin@gmail.com>
2022-12-27 20:55:30 +0400

    dev: add debug bundle

--- app/config/bundles.php
+++ app/config/bundles.php
@@ -12,4 +12,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
